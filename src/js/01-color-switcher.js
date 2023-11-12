413370eb8f89113323e8399c4fa0deaebbc3b5fb
KaterynaRadchenko <82673991+Katia-Radchenko@users.noreply.github.com>
2023-11-12 14:17:25 +0200

    color hw-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+refs.start.addEventListener('click', startColor);
+refs.stop.addEventListener('click', stopColor);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let intervalId = null;
+
+function startColor() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000)
+
+  refs.start.setAttribute('disabled', true)
+  refs.stop.removeAttribute('disabled')
+}
+
+function stopColor() {
+  clearInterval(intervalId);
+
+  refs.stop.setAttribute('disabled', true)
+  refs.start.removeAttribute('disabled')
+}
